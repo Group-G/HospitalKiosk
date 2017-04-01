4c689d3c4dba7ad344a3fa66d2dec1d1ceadd734
Alazar Genene <alazar.genene@gmail.com>
2017-04-01 14:32:24 -0400

    Floor Location classes added 2

--- src/groupg/Category.java
+++ src/groupg/Category.java
@@ -4,4 +4,17 @@ package groupg;
  * Created by AlazarGenene on 4/1/17.
  */
 public class Category {
-}
+    private String categoryName;
+    private Location location;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }}
--- src/groupg/Location.java
+++ src/groupg/Location.java
@@ -1,7 +1,73 @@
 package groupg;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 /**
  * Created by AlazarGenene on 4/1/17.
  */
 public class Location {
+
+
+
+import java.util.ArrayList;
+
+    /**
+     * Created by AlazarGenene on 4/1/17.
+     */
+
+    public class Location {
+        private int x, y, ID;
+        private Category category;
+        private String floor, building;
+        ArrayList<groupg.Location> neighbors = new ArrayList<groupg.Location>();
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setFloor(String floor) {
+            this.floor = floor;
+        }
+
+        public void setBuilding(String building) {
+            this.building = building;
+        }
+
+        public String getFloor() {
+
+            return floor;
+        }
+
+        public String getBuilding() {
+            return building;
+        }
+
+        public int getID() {
+            return ID;
+        }
+
+        public Category getCategory() {
+            return category;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public void setID(int ID) {
+            this.ID = ID;
+        }
+
+        public void setCategory(Category category) {
+            this.category = category;
+        }
+    }
+
 }
