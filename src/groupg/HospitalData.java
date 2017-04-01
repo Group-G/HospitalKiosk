e88a7c375f2d595b3162f505d29770bcdc312b9c
Saul Woolf <saulvhwoolf@gmail.com>
2017-04-01 14:33:43 -0400

    Making HospitalData

---
+++ src/groupg/HospitalData.java
@@ -0,0 +1,30 @@
+package groupg;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by svwoolf on 4/1/17.
+ */
+public class HospitalData
+{
+//    List<Building> buildings = new LinkedList<>();
+    List<String> categories = new LinkedList<>();
+    HospitalData()
+    {
+        if(pullDataFromDB())
+        {
+            System.out.println("Successfully pulled data from DB");
+        }
+        else
+        {
+            System.out.println("Failed to pull data from DB");
+        }
+    }
+
+
+    public static boolean pullDataFromDB()
+    {
+        return false;
+    }
+}
