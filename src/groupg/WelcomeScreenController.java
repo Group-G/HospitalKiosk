0d115e68de81b48810727517389954bb9ccc8ca7
Octogonapus <firey45@gmail.com>
2017-04-01 22:50:25 -0400

    Added autocomplete text field

--- src/groupg/WelcomeScreenController.java
+++ src/groupg/WelcomeScreenController.java
@@ -1,25 +1,44 @@
 package groupg;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
-import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.ResourceBundle;
 
 /**
  * @author Ryan Benasutti
  * @since 2017-03-23
  */
-public class WelcomeScreenController
+public class WelcomeScreenController implements Initializable
 {
     @FXML
     private Button adminBtn;
     @FXML
-    private TextField searchField;
+    private HBox topHBox;
     @FXML
     private MenuButton catDropdown;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        AutoCompleteTextField autoCompleteTextField = new AutoCompleteTextField();
+        autoCompleteTextField.getEntries().add("TEST 1");
+
+        ObservableList<Node> children = FXCollections.observableArrayList(topHBox.getChildren());
+        children.add(autoCompleteTextField);
+        Collections.swap(children, 0, 1);
+        topHBox.getChildren().setAll(children);
+    }
 
     public void onAdminLogin(ActionEvent actionEvent)
     {
@@ -31,10 +50,5 @@ public class WelcomeScreenController
         {
             e.printStackTrace();
         }
-    }
-
-    public void onCatSelect(ActionEvent actionEvent)
-    {
-        //Do something with catDropdown
     }
 }
