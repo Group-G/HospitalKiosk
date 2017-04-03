c9481b3a55841252ba62cd9fb46921e68ab6b34d
Ryan Benasutti <firey45@gmail.com>
2017-04-03 19:42:50 -0400

    Added login credentials

--- src/groupg/AdminLoginController.java
+++ src/groupg/AdminLoginController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
@@ -20,10 +21,13 @@ public class AdminLoginController implements Initializable
     private Button cancelBtn, loginBtn;
     @FXML
     private TextField usernameField, passField;
+    @FXML
+    private Text errorText;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
+        errorText.setVisible(false);
     }
 
     public void onCancel(ActionEvent actionEvent)
@@ -40,15 +44,33 @@ public class AdminLoginController implements Initializable
 
     public void onLogin(ActionEvent actionEvent)
     {
-        //Check credentials: usernameField, passField
+        attemptLogin(actionEvent);
+    }
 
-        try
+    public void onEnterKey(ActionEvent actionEvent)
+    {
+        attemptLogin(actionEvent);
+    }
+
+    private void attemptLogin(ActionEvent actionEvent)
+    {
+        if (usernameField.getText().equals(HospitalData.login[0]) &&
+            passField.getText().equals(HospitalData.login[1]))
         {
-            ResourceManager.getInstance().loadFXMLIntoScene("/adminMain.fxml", "Admin Main", cancelBtn.getScene());
+            errorText.setVisible(false);
+
+            try
+            {
+                ResourceManager.getInstance().loadFXMLIntoScene("/adminMain.fxml", "Admin Main", cancelBtn.getScene());
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
         }
-        catch (IOException e)
+        else
         {
-            e.printStackTrace();
+            errorText.setVisible(true);
         }
     }
 }
