e443d9d29dc2234d627f77af3d0472594f0a90c1
Saul Woolf <saulvhwoolf@gmail.com>
2017-04-03 21:39:44 -0400

    Pls NO MORE GDMAN COMMENTS

--- src/groupg/Location.java
+++ src/groupg/Location.java
@@ -19,6 +19,19 @@ public class Location implements Comparable
     private double fcost;
     private Location parent;
 
+    /*SETTERS*/
+    /**
+     * Constructor
+     * @param name
+     * @param x
+     * @param y
+     * @param neighbors
+     * @param category
+     * @param weight
+     * @param ID
+     * @param floor
+     * @param building
+     */
     public Location(String name, double x, double y, LinkedList<Integer> neighbors, String category, float weight, int ID, int floor, int building) {
         this.name = name;
         this.x = x;
@@ -31,6 +44,18 @@ public class Location implements Comparable
         this.id = ID;
 //        System.out.println("made location" + ID);
     }
+
+    /**
+     * Constructor
+     * @param name
+     * @param x
+     * @param y
+     * @param category
+     * @param weight
+     * @param ID
+     * @param floor
+     * @param building
+     */
     public Location(String name, double x, double y, String category, float weight, int ID, int floor, int building) {
         this.name = name;
         this.x = x;
@@ -43,77 +68,145 @@ public class Location implements Comparable
 //        System.out.println("made location" + ID);
     }
 
-    /*SETTERS*/
+    /**
+     * Setter for c
+     * @param x
+     */
     public void setX(int x) {
         this.x = x;
 
     }
 
+    /**
+     * Setter for Y
+     * @param y
+     */
     public void setY(int y) {
         this.y = y;
     }
 
+    /**
+     * Setter for ID
+     * @param ID
+     */
     public void setID(int ID) {
         this.id = ID;
     }
 
+    /**
+     * Setter for Category
+     * @param category
+     */
     public void setCategory(String category) {
         this.category = category;
     }
 
+    /**
+     * Setter for Floor
+     * @param floor
+     */
     public void setFloor(int floor) {
         this.floor = floor;
     }
 
+    /**
+     * Setter for building
+     * @param building
+     */
     public void setBuilding(int building) {
         this.building = building;
     }
-    /*END SETTERS*/
 
-    /*GETTERS*/
-    public double getX() {
-        return this.x;
-    }
-
-    public double getY() {
-        return this.y;
-    }
-
-    public int getFloor() {
-        return this.floor;
-    }
-
-    public int getBuilding() {
-        return this.building;
-    }
-
-    public int getID() {
-        return this.id;
-    }
-
-    public String getCategory() {
-        return this.category;
-    }
-
-    public double getFcost() {
-        return fcost;
-    }
-
-    public LinkedList<Integer> getNeighbors() {
-        return neighbors;
-    }
-    /*END GETTERS*/
-
-
-    public String getName()
-    {
-        return name;
-    }
-
+    /**
+     * Setter for name
+     * @param name
+     */
     public void setName(String name)
     {
         this.name = name;
     }
+    /*END SETTERS*/
+
+
+
+
+    /*GETTERS*/
+
+    /**
+     * Getter for X
+     * @return
+     */
+    public double getX() {
+        return this.x;
+    }
+
+    /**
+     * Getter for Y
+     * @return
+     */
+    public double getY() {
+        return this.y;
+    }
+
+    /**
+     * Getter for Floor
+     * @return
+     */
+    public int getFloor() {
+        return this.floor;
+    }
+
+    /**
+     * Getter for Building
+     * @return
+     */
+    public int getBuilding() {
+        return this.building;
+    }
+
+    /**
+     * Getter for ID
+     * @return
+     */
+    public int getID() {
+        return this.id;
+    }
+
+    /**
+     * Getter for Category
+     * @return
+     */
+    public String getCategory() {
+        return this.category;
+    }
+
+    /**
+     * Getter for Fcost
+     * @return
+     */
+    public double getFcost() {
+        return fcost;
+    }
+
+    /**
+     * Getter for neighbors
+     * @return
+     */
+    public LinkedList<Integer> getNeighbors() {
+        return neighbors;
+    }
+
+    /**
+     * Getter for name
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+    /*END GETTERS*/
+
+
+
 
     //gets length between node and this node
     public double lengthTo(Location other){
@@ -121,29 +214,55 @@ public class Location implements Comparable
                          + Math.pow(Math.abs(this.getY() - other.getY()), 2));
     }
 
+    /**
+     * ToString
+     * @return
+     */
     @Override
     public String toString()
     {
         return getName();
     }
 
+    /**
+     * Setter for FCost
+     * @param fcost
+     */
     public void setFcost(double fcost) {
         this.fcost = fcost;
     }
 
+    /**
+     * Setter for parent
+     * @param parent
+     */
     public void setParent(Location parent) {
         this.parent = parent;
     }
 
+    /**
+     * Getter for parent
+     * @return
+     */
     public Location getParent() {
         return this.parent;
     }
 
+    /**
+     * CompareTo
+     * @param o
+     * @return
+     */
     @Override
      public int compareTo(Object o)
     {
          return toString().compareTo(o.toString());
     }
+
+    /**
+     * AddNeighbor
+     * @param id
+     */
     public void addNeighbor(int id)
     {
         neighbors.add(id);
@@ -180,13 +299,19 @@ public class Location implements Comparable
         return result;
     }
 
+    /**
+     * Getter for Floor
+     * @return
+     */
     public int getFloorId() {
         return floor;
     }
 
-
-    public void setLocation(Location l)
-    {
+    /**
+     * Remake this one
+     * @param l
+     */
+    public void setLocation(Location l) {
         //String name, double x, double y, LinkedList<Integer> neighbors, String category, float weight, int ID, int floor, int building
         this.name = l.getName();
         this.x = l.getX();
@@ -199,6 +324,10 @@ public class Location implements Comparable
 
     }
 
+    /**
+     * Getter for weight
+     * @return
+     */
     public float getWeight() {
         return weight;
     }
