061c102c57af304659f7d282d308e838b131d1a4
Dylan McKillip <dylanmckillip@gmail.com>
2017-04-08 16:53:50 -0400

    added depth first algorithm

---
+++ src/groupg/algorithm/DepthFirst.java
@@ -0,0 +1,55 @@
+package groupg.algorithm;
+
+import groupg.database.Location;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Dylan on 4/7/17.
+ */
+public class DepthFirst{
+    LinkedList<Location> path = new LinkedList<>();
+    LinkedList<Location> locations = new LinkedList<>();
+    LinkedList<Location> searched = new LinkedList<>();
+
+    DepthFirst(LinkedList<Location> loc){
+        this.locations = loc;
+    }
+
+    public LinkedList<Location> run(Location start, Location end){
+        LinkedList<Location> dfpath = new LinkedList<>();
+        try{
+            dfpath = runDepthFirst(start, end);
+        }
+        catch(NullPointerException e){
+            System.out.println("Path not found");
+            return new LinkedList<>();
+        }
+        return dfpath;
+    }
+
+    private LinkedList<Location> runDepthFirst(Location start, Location end) throws NullPointerException{
+        path.clear();
+        searched.clear();
+        searched.add(start);
+        if(isReachable(start, end)){
+            return path;
+        }
+        else throw new NullPointerException();
+    }
+
+    private boolean isReachable(Location strt, Location goal){
+        searched.add(strt);
+        if(strt.equals(goal)){
+            return true;
+        }
+        for (Location neighbor: strt.getNeighbors()) {
+            if (!searched.contains(neighbor)){
+                if(isReachable(neighbor, goal)){
+                    path.add(neighbor);
+                }
+            }
+        }
+        return false;
+    }
+}
