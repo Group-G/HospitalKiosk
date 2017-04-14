fcaa29cfe78e82a94a15827eb39ae27612133cc1
LAPTOP-QI8D7EP6\dannm <dmcdonough@wpi.edu>
2017-04-14 18:35:41 -0400

    About button works but does nothing

---
+++ src/groupg/controller/AboutController.java
@@ -0,0 +1,17 @@
+package groupg.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by dannm on 4/14/2017.
+ */
+public class AboutController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
