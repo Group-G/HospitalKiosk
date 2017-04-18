d28ad71fb58805328e800d62620703239c0ea884
LAPTOP-QI8D7EP6\dannm <dmcdonough@wpi.edu>
2017-04-17 23:00:05 -0400

    permissions

--- src/groupg/controller/EditCategoryAddController.java
+++ src/groupg/controller/EditCategoryAddController.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -31,6 +32,9 @@ public class EditCategoryAddController implements Initializable
     @FXML
     private ColorPicker colorField;
 
+    @FXML
+    private RadioButton radiopublic, radioprivate;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
@@ -51,8 +55,15 @@ public class EditCategoryAddController implements Initializable
 
     public void onAdd(ActionEvent event)
     {
-        Main.h.addCategory(catNameField.getText(), 0, colorField.getValue().toString());
-
+        if(radioprivate.isArmed()) {
+            Main.h.addCategory(catNameField.getText(), 1, colorField.getValue().toString());
+        }
+        else if(radiopublic.isArmed()){
+            Main.h.addCategory(catNameField.getText(), 0, colorField.getValue().toString());
+        }
+        else{
+            Main.h.addCategory(catNameField.getText(), 0, colorField.getValue().toString());
+        }
         try
         {
             ResourceManager.getInstance().loadFXMLIntoScene("/view/editCategory.fxml", "Edit Categories", addBtn.getScene());
